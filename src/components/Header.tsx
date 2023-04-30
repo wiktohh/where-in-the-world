1e57f7cbeab1284d459593b26c5e909508929bb7
wiktohh <wiktor.rzeznicki01@gmail.com>
2023-04-30 20:41:23 +0200

    create and style header

---
+++ src/components/Header.tsx
@@ -0,0 +1,14 @@
+const Header = () => {
+    return ( 
+        <header className="header">
+            <div className="header__container">
+                <h1 className="header__container-h1">Where in the world?</h1>
+                <button className="header__container-switch-button">
+                    Dark mode
+                </button>
+            </div>
+        </header>
+     );
+}
+ 
+export default Header;
